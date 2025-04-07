4879538a949325f3959808113f2aca2f75edb3c3
Miradji-Ladhidhou <ladhidhoum@gmail.com>
2025-04-07 20:30:57 +0400

    Installation de bcrypt (ou bcryptjs)

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var jwt = require('jsonwebtoken');
-var bcrypt = require('bcryptjs');
+var bcrypt = require('bcrypt');
 require('dotenv').config(); 
 
 /**
