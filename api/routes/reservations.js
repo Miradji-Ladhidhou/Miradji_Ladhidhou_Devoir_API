d0d42af9c827ec612568182bed9b08138c889818
Miradji-Ladhidhou <ladhidhoum@gmail.com>
2025-04-07 21:36:41 +0400

    Renommage avec la bonne casse

--- api/routes/reservations.js
+++ api/routes/reservations.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Reservation = require('../models/reservation_tmp');
+var Reservation = require('../models/reservation');
 
 /**
  * Route pour afficher toutes les réservations.
