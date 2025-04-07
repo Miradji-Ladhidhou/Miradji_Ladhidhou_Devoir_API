ab5694d23301d6a3c58847ecb605cb11b3d352f0
Miradji-Ladhidhou <ladhidhoum@gmail.com>
2025-04-07 20:44:43 +0400

    Remplace bcrypt par bcryptjs et nettoie node_modules

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var jwt = require('jsonwebtoken');
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcryptjs');
 require('dotenv').config(); 
 
 /**
